6dae9efec5f31a3f72792d83fdcde9461a14e874
krzysztof.lichota <krzysztof.lichota@3d7dc5d9-1f30-0410-a0cb-d9e7f84b85d9>
2007-06-10 19:30:43 +0000

    Added dropping caches support.

--- tools/openoffice/measure_times.sh
+++ tools/openoffice/measure_times.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 OUTPUT_FILE=openoffice_times.txt
+
+THIS_SCRIPT_DIR=`dirname "$0"`
+
+. "$THIS_SCRIPT_DIR"/../toolslib.sh
+
 echo >>"$OUTPUT_FILE"
 for i in `seq 10`; do
+	if [ "$DROP_CACHES" = "yes" ]; then
+		drop_caches
+	fi
 	`dirname "$0"`/measure_openoffice_start_time.sh | tee -a "$OUTPUT_FILE"
 done
