352ebfdb725478124a0dfca5fb863a9d8bf3753c
krzysztof.lichota <krzysztof.lichota@3d7dc5d9-1f30-0410-a0cb-d9e7f84b85d9>
2007-06-10 10:20:33 +0000

    - Added failure function. - Changed gettime to get_time. - Added subtract_time function.

--- trunk/tools/toolslib.sh
+++ trunk/tools/toolslib.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 #File with common script functions.
 
+#prints message and exits script
+failure() {
+	echo "$@"
+	exit 1
+}
+
 #Returns time in format suitable for subtraction
 #Resolution is nanoseconds.
-gettime() {
+get_time() {
 	date --utc +'%s.%N'
 }
 
+#Subtracts time as returned by get_time().
+#Result is in seconds (with fractions)
+subtract_time() {
+	[ -z "$1" ] && failure "subtract_time requires 2 arguments"
+	[ -z "$2" ] && failure "subtract_time requires 2 arguments"
+	python -c "print $1 - $2"
+}
