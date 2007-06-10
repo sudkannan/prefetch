432f132067a1afd2ccbe5b16f3094883bc466b38
krzysztof.lichota <krzysztof.lichota@3d7dc5d9-1f30-0410-a0cb-d9e7f84b85d9>
2007-06-10 20:20:49 +0000

    Added hdparm -f in addition to drop_caches.

--- tools/openoffice/measure_times.sh
+++ tools/openoffice/measure_times.sh
@@ -9,6 +9,7 @@ echo >>"$OUTPUT_FILE"
 for i in `seq 10`; do
 	if [ "$DROP_CACHES" = "yes" ]; then
 		drop_caches
+		sudo hdparm -f /dev/hda
 	fi
 	`dirname "$0"`/measure_openoffice_start_time.sh | tee -a "$OUTPUT_FILE"
 done
