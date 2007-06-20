6a17ea95101ae8d7a91a16ac7871e12c3dab9f2a
krzysztof.lichota <krzysztof.lichota@3d7dc5d9-1f30-0410-a0cb-d9e7f84b85d9>
2007-06-20 10:19:12 +0000

    Added printing uname -a.

--- tools/openoffice/autorun-openoffice-startup.sh
+++ tools/openoffice/autorun-openoffice-startup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-echo Sleeping 60 seconds to quiesce after reboot
+echo Sleeping 60 to quiesce after reboot
 sleep 60
 echo Running openoffice test
 echo "#restart" >>~/openoffice_startup.txt
+echo "#" `uname -a` >>~/openoffice_startup.txt
 
 cd ~/prefetch/trunk/tools/openoffice
 ./measure_openoffice_start_time.sh >>~/openoffice_startup.txt || exit 1
