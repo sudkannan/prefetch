dcc51e3f21e94b874261eb4c17c92fbafd4e5948
krzysztof.lichota <krzysztof.lichota@3d7dc5d9-1f30-0410-a0cb-d9e7f84b85d9>
2007-07-04 10:34:32 +0000

    Added looping over values for ASYNC_PREFETCH and DROP_CACHES for easy repeatable testing.

--- trunk/tools/openoffice/measure_times.sh
+++ trunk/tools/openoffice/measure_times.sh
@@ -6,6 +6,20 @@ THIS_SCRIPT_DIR=`dirname "$0"`
 . "$THIS_SCRIPT_DIR"/../toolslib.sh
 
 echo >>"$OUTPUT_FILE"
-for i in `seq 10`; do
-	`dirname "$0"`/measure_openoffice_start_time.sh | tee -a "$OUTPUT_FILE"
+
+TEST_RUNS=5
+ASYNC_PREFETCH_VALUES="no yes"
+DROP_CACHES_VALUES="3 2 1"
+
+for ASYNC_PREFETCH in $ASYNC_PREFETCH_VALUES; do
+	export ASYNC_PREFETCH
+	echo "ASYNC_PREFETCH=$ASYNC_PREFETCH" >>"$OUTPUT_FILE"
+	
+	for DROP_CACHES in $DROP_CACHES_VALUES; do
+		echo "DROP_CACHES=$DROP_CACHES"
+		export DROP_CACHES
+		for i in `seq $TEST_RUNS`; do
+			`dirname "$0"`/measure_openoffice_start_time.sh | tee -a "$OUTPUT_FILE"
+		done
+	done
 done
