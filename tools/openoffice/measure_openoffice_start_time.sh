0cee0b5c73e3b4d7b437cb264ed3d11a0ccf6372
krzysztof.lichota <krzysztof.lichota@3d7dc5d9-1f30-0410-a0cb-d9e7f84b85d9>
2007-07-04 10:18:16 +0000

    * Added saving prefetch trace option. * Added printing io ticks and stat line in results. * Added option to set async/sync mode.

--- tools/openoffice/measure_openoffice_start_time.sh
+++ tools/openoffice/measure_openoffice_start_time.sh
@@ -4,6 +4,8 @@ THIS_SCRIPT_DIR=`dirname "$0"`
 
 . "$THIS_SCRIPT_DIR"/../toolslib.sh
 
+STAMP=`date +'%Y%m%d-%T'`
+
 RESULT_FILE=$PWD/start_time.tmp
 #remove it as we check for its existence later
 if [ -e "$RESULT_FILE" ]; then
@@ -16,6 +18,12 @@ fi
 
 if [ -z "$TRACE_IO" ]; then
 	TRACE_IO=no
+fi
+
+if [ "$ASYNC_PREFETCH" = "yes" ]; then
+	sudo bash -c "echo 'mode async'>/proc/prefetch"
+else
+	sudo bash -c "echo 'mode sync'>/proc/prefetch"
 fi
 
 export OPENOFFICE_STARTUP_HOOK="$PWD/openoffice_started.sh"
@@ -55,7 +63,9 @@ while true; do
 	fi
 	sleep 1
 done
-END_TIME=`cat "$RESULT_FILE"`
+set `cat "$RESULT_FILE"`
+END_TIME=$1
+END_IO_TICKS=$2
 
 DIFFERENCE=`subtract_time $END_TIME $START_TIME`
 
@@ -65,8 +75,8 @@ fi
 
 echoerr "End time: " `cat "$RESULT_FILE"`
 echoerr "Difference: $DIFFERENCE"
-echo "$DIFFERENCE"
-
+echoerr "IO ticks: $END_IO_TICKS"
+echo "$DIFFERENCE $END_IO_TICKS #stamp: $STAMP #all: " "$@"
 
 rm -f "$RESULT_FILE" || failure "Cannot remove $RESULT_FILE, error=$?"
 
@@ -74,5 +84,10 @@ if [ "$KILL_OPENOFFICE" = "yes" ]; then
 	# Wait a bit and kill openoffice.
 	sleep 2
 	killall soffice.bin
+	if [ "$SAVE_PREFETCH_TRACE" = "yes" ]; then
+		sleep 3
+		sudo cat /proc/prefetch >>prefetch-trace-$STAMP.txt
+	fi
+
 fi
 
