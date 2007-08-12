bf5ed3a00f9dedc81ba866b134d0ead656366806
krzysztof.lichota <krzysztof.lichota@3d7dc5d9-1f30-0410-a0cb-d9e7f84b85d9>
2007-08-12 13:34:49 +0000

    Added kernel command line to log.

--- trunk/tools/record-boot-time/record-boot-time.sh
+++ trunk/tools/record-boot-time/record-boot-time.sh
@@ -5,9 +5,10 @@ commentout() {
 
 LOG_FILE="$HOME/boot_time.log"
 echo "#restart" >>"$LOG_FILE"
-echo "#" `uname -a` >>"$LOG_FILE"
-
-ls -la /.prefetch* | commentout >>"$LOG_FILE"
-
 #record time since start
 cat /proc/uptime >>"$LOG_FILE"
+echo "#" `uname -a` >>"$LOG_FILE"
+echo "#kernel command line:" >>"$LOG_FILE"
+cat /proc/cmdline | commentout >>"$LOG_FILE"
+
+ls -la /.prefetch* | commentout >>"$LOG_FILE"
