c56d9aae709f74b5580bc87fbd87a0ef7ef13b73
krzysztof.lichota <krzysztof.lichota@3d7dc5d9-1f30-0410-a0cb-d9e7f84b85d9>
2007-08-12 15:03:36 +0000

    Added LSB info. Removed unnecessary sudoers file.

--- tools/record-boot-time/record-boot-time.sh
+++ tools/record-boot-time/record-boot-time.sh
@@ -10,5 +10,6 @@ cat /proc/uptime >>"$LOG_FILE"
 echo "#" `uname -a` >>"$LOG_FILE"
 echo "#kernel command line:" >>"$LOG_FILE"
 cat /proc/cmdline | commentout >>"$LOG_FILE"
+lsb_release -a | commentout >>"$LOG_FILE"
 
 ls -la /.prefetch* | commentout >>"$LOG_FILE"
