2a47c320eaadd3efc28220f8057c21d8028b9432
krzysztof.lichota <krzysztof.lichota@3d7dc5d9-1f30-0410-a0cb-d9e7f84b85d9>
2007-08-19 09:16:52 +0000

    Improvements to e2blockmap - printing blocks of file.

--- e2moveblocks/e2blockmap.cpp
+++ e2moveblocks/e2blockmap.cpp
@@ -215,7 +215,31 @@ inline bool is_metadata_block(e2_blkcnt_t block)
     return false;
 }
 
-int print_file_blocks_verbose(
+typedef struct {
+    ext2_ino_t inode_number;
+    e2_blkcnt_t start_offset; //block offset in file from which to start printing, <0 if not used
+    e2_blkcnt_t length;  //length in blocks of printed block, <0 to print till end of file
+    bool print_holes;
+    bool print_indirect;
+    void (*print_block_fun)(
+        blk_t blocknr, //number of block on disk
+        ext2_ino_t inode_number,
+        e2_blkcnt_t block_offset, //offset of block in file
+        blk_t ref_blk, 
+        int ref_offset
+        );
+    void (*print_indirect_fun)(
+        blk_t blocknr, 
+        ext2_ino_t inode_number,
+        e2_blkcnt_t blockcnt, 
+        blk_t ref_blk, 
+        int ref_offset
+        );
+    //private to printing function
+    bool in_printing_range;
+} print_params_t;
+
+int print_file_blocks(
     ext2_filsys fs, 
     blk_t *blocknr, 
     e2_blkcnt_t blockcnt, 
@@ -224,20 +248,39 @@ int print_file_blocks_verbose(
     void *private_data
     )
 {
+    print_params_t *params = (print_params_t*)private_data;
+    
     if (is_metadata_block(blockcnt))
     {
-        printf("type=%s diskblock=%u  ref_blk=%u ref_offset=%d\n",
-            block_type_string(blockcnt),
-            *blocknr,
-            ref_blk,
-            ref_offset
-            );
+        if (params->print_indirect && params->in_printing_range)
+        {
+            params->print_indirect_fun(
+                *blocknr,
+                params->inode_number,
+                blockcnt,
+                ref_blk,
+                ref_offset
+                );
+        }
     }
     else
     {
-        printf("blocknumber=%llu diskblock=%u ref_blk=%u ref_offset=%d\n",
-            blockcnt,
+        params->in_printing_range = false;
+        if (params->start_offset >= 0 && blockcnt < params->start_offset) 
+        {
+            //not yet in range
+            return 0;
+        }
+        if (params->length >= 0 && blockcnt >= params->start_offset + params->length)
+        {
+            //went behind range
+            return BLOCK_ABORT;
+        }
+        params->in_printing_range = true;
+        params->print_block_fun(
             *blocknr,
+            params->inode_number,
+            blockcnt,
             ref_blk,
             ref_offset
             );
@@ -258,36 +301,60 @@ int show_file_blockmap(
     filesystem_info *fs_info, 
     char const * file_name, 
     print_file_blocks_function_t print_file_blocks_function,
-    void *private_data
+    print_params_t *print_params
     )
 {
     ext2_ino_t inode_number;
     ext2_ino_t root;
     ext2_ino_t cwd;
     errcode_t retval;
+    unsigned flags = 0;
     
-    //lookup file name
-    root = cwd = EXT2_ROOT_INO;
-    retval = ext2fs_namei_follow(
-        fs_info->fs, 
-        root, 
-        cwd, 
-        file_name,
-        &inode_number
-        );
-    if (retval != 0)
+    if (file_name[0] == '/') 
     {
-        error_msg("Warning: failed to lookup file, file=%s, error=%s\n", file_name, error_message(retval));
-        return 0;
+        //lookup file name
+        root = cwd = EXT2_ROOT_INO;
+        retval = ext2fs_namei_follow(
+            fs_info->fs, 
+            root, 
+            cwd, 
+            file_name,
+            &inode_number
+            );
+        if (retval != 0)
+        {
+            error_msg("Warning: failed to lookup file, file=%s, error=%s\n", file_name, error_message(retval));
+            return 0;
+        }
+    } 
+    else if (file_name[0] >= '0' && file_name[0] <= '9')
+    {
+        //inode number
+        inode_number = atol(file_name);
+    } 
+    else 
+    {
+            error_msg("Invalid file or inode name %s\n", file_name);
+            return 0;
+    }
+    print_params->inode_number = inode_number;
+    
+    if (print_params->print_holes == true) 
+    {
+        flags |= BLOCK_FLAG_HOLE;
+    }
+    if (print_params->print_indirect == false)
+    {
+        flags |= BLOCK_FLAG_DATA_ONLY;
     }
     
     retval = ext2fs_block_iterate2(
         fs_info->fs, 
         inode_number, 
-        0, //BLOCK_FLAG_HOLE, //flags
+        flags,
         NULL, //block_buf
         print_file_blocks_function,
-        private_data
+        print_params
         );
     
     if (retval != 0) 
@@ -302,23 +369,151 @@ int show_file_blockmap(
     return 1;
 }
 
+void print_indirect_verbose(
+    blk_t blocknr, 
+    ext2_ino_t inode_number,
+    e2_blkcnt_t blockcnt, 
+    blk_t ref_blk, 
+    int ref_offset
+    )
+{
+    printf("diskblock=%u  inode=%u type=%s-indirect_number=%lld ref_blk=%u ref_offset=%d\n",
+        blocknr,
+        inode_number,
+        block_type_string(blockcnt),
+        -blockcnt,
+        ref_blk,
+        ref_offset
+        );
+}
+
+void print_block_verbose(
+    blk_t blocknr, 
+    ext2_ino_t inode_number,
+    e2_blkcnt_t blockcnt, 
+    blk_t ref_blk, 
+    int ref_offset
+    )
+{
+    printf("inode=%d blocknumber=%llu diskblock=%u ref_blk=%u ref_offset=%d\n",
+        inode_number,
+        blockcnt,
+        blocknr,
+        ref_blk,
+        ref_offset
+        );
+}
+
+void print_indirect(
+    blk_t blocknr, 
+    ext2_ino_t inode_number,
+    e2_blkcnt_t blockcnt, 
+    blk_t ref_blk, 
+    int ref_offset
+    )
+{
+    printf("%u %u %s-%lld %u %d\n",
+        blocknr,
+        inode_number,
+        block_type_string(blockcnt),
+        -blockcnt,
+        ref_blk,
+        ref_offset
+        );
+}
+
+void print_block(
+    blk_t blocknr, 
+    ext2_ino_t inode_number,
+    e2_blkcnt_t blockcnt, 
+    blk_t ref_blk, 
+    int ref_offset
+    )
+{
+    printf("%u %d %lld\n",
+        blocknr,
+        inode_number,
+        blockcnt
+        );
+}
+
 void usage()
 {
-    error_msg("Usage: e2blockmap file_name\n");
+    error_msg("Usage: e2blockmap -p params device file_or_inode_name start_offset length\n");
+    error_msg("\tparams - print parameters:\n");
+    error_msg("\t\tverbose - print descriptions\n");
+    error_msg("\t\tindirect - print indirect blocks\n");
+    error_msg("\t\tholes - print also holes in file (note that printing holes causes indirect blocks to be printed after real blocks\n");
+    error_msg("\tdevice - name of block device or file which contains filesystem\n");
+    error_msg("\tfile_or_inode_name - name of file on filesystem (starting with /) or inode number\n");
+    error_msg("\tstart_offset - offset in file from which to start printing\n");
+    error_msg("\tlength - length of block from start_offset which will be printed, -1 to print till end of file\n");
 }
 
 int main(int argc, char **argv)
 {
-    if (argc < 2)
+    if (argc != 7)
     {
         usage();
-        exit(-1);
+        exit(1);
     }
+    
+    char *param_token = argv[1];
+    char *params_str = argv[2];
+    char *devicename = argv[3];
+    char *filename = argv[4];
+    e2_blkcnt_t start_offset = atol(argv[5]);
+    e2_blkcnt_t length = atol(argv[6]);
+    
+    if (strcmp(param_token, "-p") != 0)
+    {
+        usage();
+        exit(1);
+    }
+    print_params_t print_params;
+    print_params.start_offset = start_offset; //means not used
+    print_params.length = length; //means not used
+    print_params.print_holes = false;
+    print_params.print_indirect = false;
+    print_params.print_block_fun = print_block;
+    print_params.print_indirect_fun = print_indirect;
+    print_params.in_printing_range = false;
+    
+    char *p = params_str;
+    for (;;) 
+    {
+        char *param = strtok(p, ", ");
+        if (param == NULL)
+        {
+            break;
+        }
+        p = NULL;
+        if (strcmp(param, "verbose") == 0)
+        {
+            print_params.print_block_fun = print_block_verbose;
+            print_params.print_indirect_fun = print_indirect_verbose;
+        }
+        else if (strcmp(param, "indirect") == 0)
+        {
+            print_params.print_indirect = true;
+        } 
+        else if (strcmp(param, "holes") == 0)
+        {
+            print_params.print_holes = true;
+        } 
+        else
+        {
+            error_msg("Unknown print parameter: %s\n", param);
+            usage();
+            exit(1);
+        }
+    }
+    
 
     initialize_ext2_error_table();
     
     filesystem_info fs_info;
-    if (!open_filesystem("ext3.img", &fs_info))
+    if (!open_filesystem(devicename, &fs_info))
     {
         exit(3);
     }
@@ -329,9 +524,9 @@ int main(int argc, char **argv)
     
     show_file_blockmap(
         &fs_info,
-        argv[1], 
-        print_file_blocks_verbose,
-        NULL //private_data
+        filename, 
+        print_file_blocks,
+        &print_params
         );
     
     close_filesystem(&fs_info);
